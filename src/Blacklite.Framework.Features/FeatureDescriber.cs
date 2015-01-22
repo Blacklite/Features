c6ddee86d6b4f3c1520b83a8d5b9ff0a94928ddb
David Driscoll <david.driscoll@gmail.com>
2015-01-22 07:27:06 -0500

    Added feature dependency describers

--- src/Blacklite.Framework.Features/FeatureDescriber.cs
+++ src/Blacklite.Framework.Features/FeatureDescriber.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Blacklite.Framework.Features
 {
-    public class FeatureDescriber
+    public interface IFeatureDescriber
     {
+        IFeature Feature { get; }
+        IEnumerable<IFeatureDependencyDescriber> DependsOn { get; }
+        IEnumerable<IFeatureDependencyDescriber> Contains { get; }
+    }
 
+    class FeatureDescriber : IFeatureDescriber
+    {
+        public IFeature Feature { get; }
+        public IEnumerable<IFeatureDependencyDescriber> DependsOn { get; }
+        public IEnumerable<IFeatureDependencyDescriber> Contains { get; }
+    }
+
+    public interface IFeatureDependencyDescriber
+    {
+        IFeature Feature { get; }
+        bool IsEnabled { get; }
+    }
+
+    class FeatureDependencyDescriber : IFeatureDependencyDescriber
+    {
+        public IFeature Feature { get; }
+        public bool IsEnabled { get; }
     }
 }
